f785b4fbec25946e2318b7a1505a492e54e854fd
Krummhaus <jiri.kalina.jr@gmail.com>
2021-09-02 10:26:53 +0200

    genrl commit

--- init.el
+++ init.el
@@ -1,19 +1,5 @@
 ;; --------------------------------------------------------------
-;; Dislabling menu bar
-;; --------------------------------------------------------------
-(setq inhibit-startup-message t)
-
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-
-(menu-bar-mode -1)            ; Disable the menu bar
-
-;; --------------------------------------------------------------
-;; Package inicialize
-;; --------------------------------------------------------------
-;; Initialize package sources
+(setq user-emacs-directory "/home/mlboy/.emacs.d")
 (require 'package)
 
 (setq package-archives
@@ -38,178 +24,24 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Hint package
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-
-;; --------------------------------------------------------------
-;; INITIAL MINIMAL SETTIGS
-;; --------------------------------------------------------------
-
-;; Set up the visible bell
-(setq visible-bell t)
-
-;; Theme on start-up
-;; wombat-theme
-(load-theme 'wombat)
-
-;; Line-numbers
-(global-display-line-numbers-mode t)
-;; Disable numbering for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-number-mode 0))))
-
-;; Column nubers
-;; (column-number-mode)
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; getting rid of the "yes or no" prompt and replace it with "y or n"
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; disable confirmation if a file or buffer does not exist when you
-;; use C-x C-f or C-x b
-(setq confirm-nonexistent-file-or-buffer nil)
-
-;; disable confirmation when kill a buffer with a live process
-;; attached to it
-(setq kill-buffer-query-functions
-  (remq 'process-kill-buffer-query-function
-        kill-buffer-query-functions))
-
-;; store all backup and autosave files in the tmp dir
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-;; confirm to quit
-(setq confirm-kill-emacs #'y-or-n-p)
-
-
-;; --------------------------------------------------------------
-;; org-mode
-;; --------------------------------------------------------------
-(setq-default org-display-custom-times t)
-(setq org-time-stamp-custom-formats 
-  '("<%d %b %Y %a" . "<%d %b %Y %a %H:%M>"))
-
-;; --------------------------------------------------------------
-;; dockerfile-mode
-;; --------------------------------------------------------------
-(use-package dockerfile-mode
-  :ensure t)
-
-;; --------------------------------------------------------------
-;; Magit - GIT
-;; --------------------------------------------------------------
-(use-package magit
-  :ensure t
-  :config
-  (setq magit-completing-read-function 'ivy-completing-read)
-  :diminish auto-revert-mode)
-
-
-;; --------------------------------------------------------------
-;; EVIL - MODE --> Vim key bindings
-;; --------------------------------------------------------------
 (use-package evil
   :ensure t
   :config
   (evil-mode 1))
 
-(defun xwl-jj-as-esc ()
-  (interactive)
-  (if (memq evil-state '(insert replace))
-      (let ((changed? (buffer-modified-p)))
-          (insert "j")
-          (let* ((tm (current-time))
-                 (ch (read-key)))
-            (if (and (eq ch ?j)
-                     (< (time-to-seconds (time-since tm)) 0.5))
-                (save-excursion
-                  (delete-char -1)
-                  (evil-force-normal-state)
-                  (set-buffer-modified-p changed?))
-              (insert ch))))
-    (call-interactively 'evil-next-line)))
-
-(define-key evil-insert-state-map  "j" 'xwl-jj-as-esc)
-(define-key evil-replace-state-map "j" 'xwl-jj-as-esc)
-
-(use-package evil-collection
-  :ensure t
-  :after evil
-  :init
-  (evil-collection-init))
-
-;; --------------------------------------------------------------
-;; Ivy, Counsel and Projectile
-;; --------------------------------------------------------------
-
-;; ivy
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-;; projectile
-(use-package projectile
-  :ensure t
-  :config
-  (projectile-global-mode)
-  (setq projectile-mode-line
-        '(:eval (format " [%s]" (projectile-project-name))))
-  (setq projectile-remember-window-configs t)
-  (setq projectile-completion-system 'ivy))
-
-;; counsel
-(use-package counsel
+(use-package org
   :ensure t)
 
-;; --------------------------------------------------------------
-;; STATUS LINE
-;; --------------------------------------------------------------
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 9)))
-
-;; --------------------------------------------------------------
-;; PYTHON
-;; --------------------------------------------------------------
-(use-package elpy
-  :ensure t
-  :init
-  (elpy-enable))
-
-;; --------------------------------------------------------------
+;; Change to the path where you cloned the config to
+(org-babel-load-file "/home/mlboy/.emacs.d/myinit.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-minibuffer-history-key "M-p")
- '(package-selected-packages (quote (counsel use-package ivy evil))))
+ '(package-selected-packages
+   (quote
+    (evil-org org-superstar elpy magit helm which-key evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

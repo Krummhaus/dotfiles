e21e636b5186022e9cf9c2d2556c52cc1ce01369
krummhaus <jiri.kalina.jr@gmail.com>
2022-12-15 20:44:45 +0100

    Org Roam Babel forLinux

---
+++ lnx-init.el
@@ -0,0 +1,115 @@
+(setq make-backup-files nil)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Evil Mode
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+  (use-package evil-leader
+    :ensure t
+    :config
+    (global-evil-leader-mode t)
+    (evil-leader/set-leader "<SPC>"))
+
+  ;(use-package evil-surround
+    ;:ensure t
+    ;:config (global-evil-surround-mode))
+
+  (use-package evil-org
+    :ensure t
+    :config
+    (evil-org-set-key-theme '(textobjects insert navigation additional shift todo heading))
+    (add-hook 'org-mode-hook (lambda () (evil-org-mode))))
+  )
+
+
+(defun xwl-jj-as-esc ()
+  (interactive)
+  (if (memq evil-state '(insert replace))
+      (let ((changed? (buffer-modified-p)))
+          (insert "j")
+          (let* ((tm (current-time))
+                 (ch (read-key)))
+            (if (and (eq ch ?j)
+                     (< (time-to-seconds (time-since tm)) 0.2))
+                (save-excursion
+                  (delete-char -1)
+                  (evil-force-normal-state)
+                  (set-buffer-modified-p changed?))
+              (insert ch))))
+    (call-interactively 'evil-next-line)))
+
+(define-key evil-insert-state-map  "j" 'xwl-jj-as-esc)
+(define-key evil-replace-state-map "j" 'xwl-jj-as-esc)
+
+
+;; Org-Roam
+(use-package org-roam
+  :ensure t
+  :custom
+ (org-roam-directory (file-truename "~/org-roam"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-setup)
+  (org-roam-db-autosync-mode))
+
+(use-package yasnippet
+    :config
+    (setq yas-snippet-dirs '("~/yasnippets"))
+    (yas-global-mode 1))
+
+;; Org-Mode
+  (use-package org
+    :ensure t
+    :config
+    ;; Make sure org file code highlights correctly
+    (setq org-src-fontify-natively t)
+    (setq org-todo-keywords '(
+;;(sequence "TODO" "In Progress" "|" "Waiting" "DONE" "Completed")
+			      (sequence "Queue" "Working On" "On Hold" "|" "Finished" "Worked On" "Removed")))
+
+    (evil-leader/set-key-for-mode 'org-mode
+      "m i" 'org-clock-in
+      "m C" 'org-toggle-checkbox
+      "m o" 'org-clock-out
+      "m c" 'org-clock-jump-to-current-clock
+      "m d" 'org-clock-display
+      "m e" 'org-set-effort
+      "m p" 'org-pomodoro
+      "m t" 'org-set-tags-command
+      "m m" 'org-clock-modify-effort-estimate
+      "m s" 'org-schedule)
+
+    ;; ;; Org Agenda stuff
+;;    (setq org-agenda-files '("~/nextcloud/emacs/org/archive/work.org"))
+
+    (defun org-code (start end)
+      "Wraps selected text with org code tags"
+      (interactive "r")
+      (let ((selected-text (buffer-substring (mark) (point)))
+	    (db-name (read-from-minibuffer "Language[common-lisp]: ")))
+	(when (eq db-name "")
+	  (setf db-name "common-lisp"))
+	(kill-region start end)
+	(insert (format "#+BEGIN_SRC %s \n%s \n#+END_SRC" db-name selected-text))))
+)
+
+;;Org-Babel
+(org-babel-do-load-languages
+	 'org-babel-load-languages
+	 '((C . t)
+	 (cpp . nil)
+	   ;; other languages..
+))

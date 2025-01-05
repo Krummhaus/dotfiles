44c2ddc419bc53f8b19c1743e3f9650ad5dd1885
Krummhaus <jiri.kalina.jr@gmail.com>
2025-01-05 16:40:58 +0100

    bash, yas

--- init.el
+++ init.el
@@ -26,6 +26,20 @@
 ;;; Brackets
 (electric-pair-mode 1)
 
+
+;;; Colouring 'man' pages =====
+;;  Add the follows to your init file and use M-x man or the man command in
+;;  eshell to view man pages: (the colors fits the wombat theme; you can change them for yourself)
+(require 'man)
+(set-face-attribute 'Man-overstrike nil :inherit 'bold :foreground "orange red")
+(set-face-attribute 'Man-underline nil :inherit 'underline :foreground "forest green")
+
+;;Or to be theme agnostic:
+;(require 'man)
+;(set-face-attribute 'Man-overstrike nil :inherit font-lock-type-face :bold t)
+;(set-face-attribute 'Man-underline nil :inherit font-lock-keyword-face :underline t
+;;; =====
+
 ;;; Line
 ;; Line Numbers
 ;(global-display-line-numbers-mode)
@@ -58,8 +72,11 @@
   :ensure t
   :init
   (setq evil-want-keybinding nil)
+  :custom
+  (evil-symbol-word-search t)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+  (evil-set-undo-system 'undo-redo))
 
 ;; Evil imap jj <Esc>
 (defun xwl-jj-as-esc ()
@@ -85,7 +102,7 @@
 (evil-set-initial-state 'dired-mode 'emacs)
 (evil-set-initial-state 'magit-mode 'emacs)
 
-;;; Custom key bindings
+;;;  === Custom key bindings ====
 (global-set-key (kbd "<C-tab>") 'other-window)
 (global-set-key (kbd "C-,") 'dupl-line)
 (global-set-key (kbd "C-5") 'compile)
@@ -107,6 +124,7 @@
 ;; "M-x" remap to "Shift + Space"
 (global-set-key (kbd "S-SPC") 'execute-extended-command)
 
+;;;  =============================
 
 ;;; Unicode Everywhere
 (setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
@@ -158,7 +176,66 @@
   :ensure t)
 
 ;;; Ido Mode
-(ido-mode t)
+(use-package ido
+  :config
+  (ido-mode t)
+  (setq ido-everywhere t)
+  (setq ido-enable-flex-matching t)
+  (setq ido-all-frames nil)
+  )
+
+;;; --- Company ---
+(use-package company
+  :init
+  (global-company-mode)
+  (add-hook 'emacs-lisp-mode-hook 'company-mode)
+  :ensure t
+  :config
+  (company-mode)
+  (setq company-idle-delay 0.2)
+  (setq minimum-prefix-length 2)
+  (setq company-global-modes '(not processing-mode text-mode)) ;; Not use company on those modes
+  (add-to-list 'company-backends 'company-c-headers) ;; Backend for header files
+  (add-to-list 'company-backends 'company-elisp)
+  (when (boundp 'company-backends)
+    (make-local-variable 'company-backends)
+    ;; remove
+    (setq company-backends (delete 'company-clang company-backends))
+    ;; add
+    ;;(add-to-list 'company-backends 'company-dabbrev)
+    )
+  
+  :bind (:map company-search-map  
+              ("C-t" . company-search-toggle-filtering)
+              ("C-j" . company-select-next)
+              ("C-k" . company-select-previous)
+              :map company-active-map
+              ("C-j" . company-select-next)
+              ("C-k" . company-select-previous)))
+
+;;; Ya Snippets
+(use-package yasnippet                  ; Snippets
+  :ensure t
+  :config
+  ;; Set custom snippets path based on the system type
+  (setq yas-snippet-dirs
+        (list (if (eq system-type 'windows-nt)
+                  "c:\\dotfiles\\snippets"
+                "/home/krumm/dotfiles/snippets")))
+
+  ;; Configure yasnippet settings
+  (setq yas-verbosity 1)                ; No need to be so verbose
+  (setq yas-wrap-around-region t)       ; Wrap snippets around region
+
+  ;; Reload snippets after setting paths
+  (yas-reload-all)
+
+  ;; Enable yasnippet globally
+  (yas-global-mode))
+
+;;(use-package yasnippet-snippets         ; Collection of snippets
+  ;;:ensure t)
+
 
 ;; Define the duplicate-line function
 (defun dupl-line ()
@@ -183,7 +260,6 @@
 
 ;; Add the hook to the before-save-hook
 (add-hook 'before-save-hook 'clang-format-on-save)
-
 
 ;;; Org-Mode
 (use-package org
@@ -214,12 +290,14 @@
    ;; Add other languages as needed
    ))
 
+(use-package emacsql
+  :ensure t)
 
 ;;; Org-Roam
 (use-package org-roam
   :ensure t
   :custom
-  (org-roam-diretory
+  (org-roam-directory
    (if (eq system-type 'windows-nt)
        "c:\\dotfiles\\roam\\inbox"
        "/home/krumm/dotfiles/roam/inbox"))
@@ -246,7 +324,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit markdown-mode evil-collection gruber-darker-theme evil-org evil-leader)))
+   '(yasnippet company company-mode lsp-mode which-key treemacs winum marginalia orderless vertico org-roam markdown-mode magit gruber-darker-theme evil-org evil-leader annalist)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
